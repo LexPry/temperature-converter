45018f591193a27e9c868803f65662535c0f9ef6
Alexander <akexboswell8@gmail.com>
2022-09-22 17:35:09 -0400

    Created an Angular temperature conversion app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConversionInputComponent } from './components/conversion-input/conversion-input.component';
+import { ConversionResultComponent } from './components/conversion-result/conversion-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversionInputComponent,
+    ConversionResultComponent
   ],
   imports: [
     BrowserModule
